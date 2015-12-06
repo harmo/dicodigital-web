ffdee06624309060d17427e10a2f44348b491d23
Enguerran <engcolson@gmail.com>
2015-12-06 18:40:41 +0100

    static style

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,16 @@ var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
 var connect = require('gulp-connect');
+var cssnext = require("gulp-cssnext")
+
+gulp.task("stylesheets", function() {
+  gulp.src("src/css/index.css")
+    .pipe(cssnext({
+        compress: true
+    }))
+    .pipe(gulp.dest("./dist/css"))
+    .pipe(connect.reload());
+});
 
 gulp.task('build', function() {
   return browserify({entries: 'src/app.js', extensions: ['.js'], debug: true})
@@ -13,9 +23,10 @@ gulp.task('build', function() {
     .pipe(connect.reload());
 });
 
-gulp.task('watch', ['build'], function() {
+gulp.task('watch', ['stylesheets', 'build'], function() {
   gulp.watch('src/**/*.js', ['build']);
   gulp.watch('*.html', ['livereload']);
+  gulp.watch('src/**/*.css', ['stylesheets']);
 });
 
 gulp.task('connect', function() {
--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import WordList from './components/wordlist';
 
-ReactDOM.render(<WordList url="https://dicodigital-api.herokuapp.com/word/?format=json" />, document.getElementById('wordList'));
+// ReactDOM.render(<h1>Hello</h1>, document.getElementById('reac'));
